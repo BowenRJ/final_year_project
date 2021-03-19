f103097af0afb7a6cba66bd50612821ba57e98a2
Ryan Bowen <sc17r2jb@leeds.ac.uk>
2021-03-19 13:18:45 +0000

    Read OLID training data

--- src/main.py
+++ src/main.py
@@ -4,5 +4,26 @@ import pandas
 #import PyTorch
 import pickle
 #import Jupyter
+import csv
+import sklearn
 
-print("hello world")
+def readTrainingData(filePath):
+    """Read training data and returns it as a 2D array.
+
+    Array is of the form:
+    [[id, tweet string, level a, level b, level c]]
+    With the levels being the classification of the tweets.
+    """
+
+    print("\nReading data from " + filePath + '\n')
+    trainingData = []
+    with open(filePath, newline='\n', encoding="utf8") as tsvFile:
+        dataReader = csv.reader(tsvFile, delimiter='\t')
+        next(dataReader)    # Skip the header row.
+        for row in dataReader:
+            trainingData.append(row)
+    return trainingData
+
+
+
+trainingData = readTrainingData("../data/OLIDv1.0/olid-training-v1.0.tsv")
