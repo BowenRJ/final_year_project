c3147441fa7ef35138700750a77c6dc5253a9786
Ryan Bowen <sc17r2jb@leeds.ac.uk>
2021-04-16 13:15:45 +0100

    Add function for reading test data

--- src/predict.py
+++ src/predict.py
@@ -20,8 +20,27 @@ def printResults(y_test, y_pred, predictingTime):
     print("\nConfusion matrix:\n", sklearn.metrics.confusion_matrix(y_test, y_pred))
     print("\nClassification report:\n", sklearn.metrics.classification_report(y_test, y_pred))
 
-def readTestData(filen):
-    print("to implement")
+def readTestData(testData, testTargets):
+    """
+    Read test data from 2 files, and return as 2 separate lists, the data and the targets.
+    """
+    # Read data.
+    data = []
+    with open(testData, newline='\n', encoding="utf8") as tsvFile:
+        dataReader = csv.reader(tsvFile, delimiter='\t')
+        next(dataReader)    # Skip the header row.
+        for row in dataReader:
+            #print(row)
+            data.append(row[1])
+    # Read target.
+    target = []
+    with open(testTargets, newline='\n', encoding="utf8") as csvFile:
+        dataReader = csv.reader(csvFile, delimiter=',')
+        for row in dataReader:
+            #print(row)
+            target.append(row[1])
+            #print(row[1])
+    return data, target
 
 def readTrainingData(filepath):
     data   = []
@@ -34,13 +53,17 @@ def readTrainingData(filepath):
             target.append(row[2])
     return data, target
 
-####### CURRENTLY JUST USING TRAINING DATA.
 
 # Read data and target.
-data, target = readTrainingData("../data/cleansedOLID/training08-04-21.tsv")
+data, target = readTestData("../data/OLIDv1.0/testset-levela.tsv", "../data/OLIDv1.0/labels-levela.csv")
 
 # Convert data to bag of words.
-vectorizer = sklearn.feature_extraction.text.CountVectorizer(max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))
+vectorizer = sklearn.feature_extraction.text.CountVectorizer(
+    max_features=1500, 
+    min_df=5, 
+    max_df=0.7, 
+    stop_words=stopwords.words('english')
+)
 vectorizedData = vectorizer.fit_transform(data).toarray()
 
 # Split data
@@ -51,8 +74,8 @@ classifier = loadModel("../models/tree07-04-21.pickle")
 
 # Predict
 startTime = time.time()
-y_pred = classifier.predict(x_test)
+#y_pred = classifier.predict(x_test)
 
 predictingTime = time.time() - startTime
 
-printResults(y_test, y_pred, predictingTime)
+#printResults(y_test, y_pred, predictingTime)
