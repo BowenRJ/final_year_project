93895c530b706b9055888652c1c21e519223c4f6
Ryan Bowen <sc17r2jb@leeds.ac.uk>
2021-04-26 15:26:06 +0100

    Vectorize both training and testing data at once

--- src/vectorize.py
+++ src/vectorize.py
@@ -10,7 +10,7 @@ def readCleansedData(filepath):
     Read cleansed data.
     """
 
-    print("\nReading cleansed data from " + filepath + '\n')
+    print("Reading cleansed data from " + filepath)
 
     with open (filepath, "rb") as pickleFile:
         cleansedData = pickle.load(pickleFile)
@@ -22,7 +22,7 @@ def saveVectorizedData(data, filepath):
     Write vectorized data to a specified file location.
     """
 
-    print("\nWriting vectorized data to " + filepath + '\n')
+    print("Writing vectorized data to " + filepath)
     with open(filepath, "wb") as pickleFile:
         pickle.dump(data, pickleFile)
 
@@ -41,33 +41,40 @@ def BERT(data):
     print("not implemented")
 
 
+filenameSuffix = "4"
 startTime = time.time()
 
 # Read cleansed data.
-cleansedData = readCleansedData("../data/cleansedOLID/testing1.pickle")
-#cleansedData = readCleansedData("../data/cleansedOLID/training1.pickle")
+cleansedTrainingData = readCleansedData("../data/cleansedOLID/training1.pickle")
+cleansedTestingData  = readCleansedData("../data/cleansedOLID/testing1.pickle")
+cleansedData = cleansedTrainingData + cleansedTestingData
 
-# Vectorize data by bag of words method.
+
+# Create count vectorizer on all present data.
 vectorizer = sklearn.feature_extraction.text.CountVectorizer(
-        max_features=300,
+        max_features=800,
         min_df=5,
         max_df=0.7,
         stop_words=stopwords.words('english')
 )
-vectorizedData = vectorizer.fit_transform(cleansedData).toarray()
+vectorizer = vectorizer.fit(cleansedData)
 
 # tf-idf (term frequency-inverse document frequency).
 #vectorizedData = TfidfTransformer().fit_transform(vectorizedData).toarray()
+
+# Vectorize training and testing data.
+#vectorizedData = vectorizer.fit_transform(cleansedData).toarray()
+vectorizedTrainingData = vectorizer.transform(cleansedTrainingData) 
+vectorizedTestingData  = vectorizer.transform(cleansedTestingData) 
 
 
 # Save data.
 # This may be dependant on method of vectorizing? Let's wait till all implemented,
 # And then can consider making these separate function things.
-
-#saveVectorizedData(vectorizedData, "../data/vectorizedOLID/testset-levelaBagOfWords-150features_18-04-21.pickle")
-saveVectorizedData(vectorizedData, "../data/vectorizedOLID/testing2.pickle")
+saveVectorizedData(vectorizedTrainingData, "../data/vectorizedOLID/training"+filenameSuffix+".pickle")
+saveVectorizedData(vectorizedTestingData,  "../data/vectorizedOLID/testing"+filenameSuffix+".pickle")
 
 # Print time.
 endTime = time.time()
-vectorizingTime = startTime - endTime
-print("Time taken to vectorize "+str(len(cleansedData))+" items of data: "+vectorizingTime)
+vectorizingTime = endTime - startTime
+print("Time taken to vectorize "+str(len(cleansedData))+" items of training/testing data: "+str(vectorizingTime))
